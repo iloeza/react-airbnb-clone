02715aee773ad5d12d6d3aa4d09486ec20cb4662
Ivan Loeza <ijlq_loeza@hotmail.com>
2021-10-11 19:44:02 -0500

    Fix alignment in flex button text

--- src/components/styles/header/FlexSearch.styled.js
+++ src/components/styles/header/FlexSearch.styled.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const FlexSearchContainer = styled.div`
-  height: 30vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  text-align: center;
+  padding: 30px 0;
 `;
 
 export const FlexSearchButton = styled.div`
