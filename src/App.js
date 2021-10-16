12b85faeec75bcdab1a8dab99117b2ea9749b311
jramirez99 <javierrk99@gmail.com>
2021-10-15 22:58:03 -0500

    importing and adding component accommodation in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import { Header } from './components/home/header/Header';
+import { Accommodation } from './components/home/accommodation/Accommodation';
 
 export const App = () => {
     return (
         <>
             <Header />
+
+            <Accommodation />
         </>
     )
 };
