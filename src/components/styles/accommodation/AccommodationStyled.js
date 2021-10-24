eecda5632b7a8485dcba49f2e31afdd371bcd927
jramirez99 <javierrk99@gmail.com>
2021-10-24 13:05:10 -0500

    add styles

--- src/components/styles/accommodation/AccommodationStyled.js
+++ src/components/styles/accommodation/AccommodationStyled.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
-import {
-    devices,
-} from '../../styles/mediaqueries/MediaQueriesStyled';
+import { devices } from '../base/MediaQueriesStyled';
+import { 
+        grid, 
+        fColumn, 
+        flex 
+} from '../base/MixinsStyled';
 
 export const H2 = styled.h2`
     margin: 0;
@@ -11,30 +14,24 @@ export const H2 = styled.h2`
 `;
 
 export const AccommodationContainer = styled.div`
-    display: grid;
-    grid-template-columns: repeat( 2, 1fr );
-    column-gap: 1rem;
+    ${ grid( 2, '1rem' ) };
 
     img {
         border-radius: .7rem;
     }
-    
-    ${ devices.tablet`
-        grid-template-columns: repeat( 3, 1fr );
-    
-        img {
-            width: 4rem;
-        }
-    `}
 
-    ${ devices.laptop`
-        grid-template-columns: repeat( 4, 1fr );
-    `}
+    ${ devices.tablet(`
+            ${ grid( 3, '1rem' ) }; 
+            img { 
+                width: 4rem; 
+            } 
+        `)
+    }
+    ${ devices.laptop( `${ grid( 4, '1rem' ) }` ) }
 `;
 
 export const  AccommodationBox = styled.div`
-    display: flex;
-    flex-direction: column;
+    ${ fColumn };
     align-items: center;
 
     ${ devices.tablet`
@@ -44,11 +41,7 @@ export const  AccommodationBox = styled.div`
     a {
         margin-top: 1rem;
         color: black;
-
-        ${ devices.tablet`
-            display: flex;
-            gap: 1rem;
-        `}
+        ${ devices.tablet( `${ flex( '1rem' ) }` ) }
     }
 `;
 
@@ -62,11 +55,11 @@ export const Info = styled.div`
     .horas {
         font-size: 1rem;
     }
-
-    ${ devices.tablet`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        text-align: left;
-    `}
+    ${ devices.tablet` 
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            text-align: left;  
+        `
+    }
 `;
